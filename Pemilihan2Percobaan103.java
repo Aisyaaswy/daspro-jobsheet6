c42ec8d6bd8b40a94c81d5d66106bc72d667c3a8
aisyaaswy <aisyaidha@gmail.com>
2024-10-03 23:57:02 +0700

    update perocobaan 1

--- Pemilihan2Percobaan103.java
+++ Pemilihan2Percobaan103.java
@@ -7,11 +7,18 @@ public class Pemilihan2Percobaan103 {
 
         System.out.print("Masukkan Tahun : ");
         tahun = input03.nextInt();
-        if (tahun % 4 == 0) {
-            if (tahun % 100 != 0) {
-                System.out.println("Tahun Kabisat");
-        } else
-            System.out.println("Bukan Tahun Kabisat");
-        }
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+            System.out.println("Tahun Kabisat");
+            } else {
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            }
+        } else 
+        System.out.println("Bukan Tahun Kabisat");
     }
 }
--- trialjs6.java
+++ trialjs6.java
@@ -8,11 +8,14 @@ public class trialjs6 {
         System.out.print("Masukkan Tahun : ");
         tahun = sc.nextInt();
 
-        if ((tahun % 4 ) == 0 ) 
+        if ((tahun % 4 ) == 0 ) {
             if ((tahun % 100) != 0) {
-                if ((tahun % 400) == 0) 
-                     System.out.println("Tahun Kabisat");
-        } else 
+                System.out.println("Tahun Kabisat");
+            } else {
             System.out.println("Bukan Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
     }
 }
